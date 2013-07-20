a065aed1a1497d09ba0f290a0cfbb15f02f95e60
danilobrinu <ddbn.c2@gmail.com>
2013-07-20 16:13:21 -0500

    mas cambios

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['password'] = '2k258mlkml';
+$db['default']['database'] = 'db_ci_auth';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
